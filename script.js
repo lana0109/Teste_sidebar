467b9afa8deb5c829b77dcf6592acb25293f918d
lana0109 <lanasoarez2024@gmail.com>
2026-04-05 02:52:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+function toggleSidebar(){
+document.querySelector('.sidebar').classList.toggle('active');
+document.querySelector('.overlay').classList.toggle('active');
+}
+
+function done(el){
+el.classList.toggle('done');
+let list = document.getElementById('taskList');
+list.appendChild(el);
+}
